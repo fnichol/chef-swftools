5890cd2103ceddbe0e0b46b3a688dc5863809ea7
Ryan S. Northrup <rnorthrup@newleaders.com>
2014-12-08 01:46:07 -0800

    Fix 'name' and 'recipe's in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,13 @@
+name             "swftools"
 maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs SWFTools: utilities for working with Adobe Flash files (SWF files)."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.5"
+version          "0.2.6"
 
-recipe "swftools",                  "Installs SWFTools."
-recipe "swftools::apt_repository",  "Adds an apt repository (in Ubuntu) containing the swftools pacakge."
+recipe "default",         "Installs SWFTools."
+recipe "apt_repository",  "Adds an apt repository (in Ubuntu) containing the swftools pacakge."
 
 supports "ubuntu"
 
